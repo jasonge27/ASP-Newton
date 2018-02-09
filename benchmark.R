52440a586548288428126d6ca8b419b7fe208e49
Jian Ge <jiange@princeton.edu>
2018-02-08 22:07:58 -0500

    adjusting for precision

--- benchmark.R
+++ benchmark.R
@@ -16,7 +16,7 @@ sim_ic <- generate_sim_lognet(n=1000, d=5000, 0.8)
 sim_wc <- generate_sim_lognet(n=1000, d=5000, 0.2) 
 }
 
-if (FALSE){
+if (TRUE){
 load("datasets/madelon/madelon.RData")
 madelon$X[which(is.na(madelon$X))] <- 0 # missing values
 madelon$X <- madelon$X[ ,find_nonconstant_column(madelon$X)]
@@ -42,11 +42,11 @@ if (TRUE){
 # Comparisons of Running Time Across Different Methods on Logistic Regression Tasks.
 cat("===========madelon==========\n")
 prec = list(picasso=5.0*1e-6, ncvreg=1e-2, glmnet=5*1e-5 )
-#test_lognet(madelon, prec)
+test_lognet(madelon, prec)
 
 cat("===========gisette==========\n")
 prec = list(picasso=1.0*1e-4, ncvreg=1e-2, glmnet=5*1e-5 )
-#test_lognet(gisette, prec)
+test_lognet(gisette, prec)
 
 #cat("===========farmdata==========\n")
 #prec = list(picasso=1.0*1e-4, ncvreg=1e-2, glmnet=1*1e-5 )
