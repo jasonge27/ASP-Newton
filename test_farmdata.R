26c6a02b7c7a947fb6ab8068234a6829aa1e74d4
Jian Ge <jiange@princeton.edu>
2018-02-09 00:54:06 -0500

    test scripts for farmads dataset

---
+++ test_farmdata.R
@@ -0,0 +1,30 @@
+args = commandArgs(trailingOnly=TRUE)
+
+# loading and installing required packages
+library(Rcpp)
+library(glmnet)
+library(picasso)
+library(gcdnet)
+library(spams)
+library(ncvreg)
+
+source("scripts.R")
+sourceCpp("utils.cpp")
+
+
+
+if (TRUE){
+load("datasets/farmads/farmads.RData")
+farmdata$Y <- (farmdata$Y == -1)
+farmdata$X <- as.matrix(farmdata$X)
+farmdata$X <- farmdata$X[ ,find_nonconstant_column(farmdata$X)]
+farmdata$X <- scale(farmdata$X)
+save(farmdata, file="datasets/farmads/farmads.RData")
+}
+
+
+nlambda = as.integer(args[1])
+ratio = as.double(args[2]) 
+
+prec = list(picasso=1.0*1e-4, ncvreg=1e-2, glmnet=1*1e-5 )
+test_lognet(farmdata, prec, nlambda=nlambda, ratio=ratio, skip=c('ncvreg', 'gcdnet', 'fista'))
---
+++ test_farmdata.sh
@@ -0,0 +1,6 @@
+Rscript test_farmdata.R 100 0.01 > out_1.txt &
+Rscript test_farmdata.R 100 0.1 > out_2.txt &
+Rscript test_farmdata.R 100 0.05 > out_3.txt &
+Rscript test_farmdata.R 20 0.1 > out_4.txt &
+Rscript test_farmdata.R 20 0.01 > out_5.txt &
+Rscript test_farmdata.R 20 0.05 > out_6.txt 
